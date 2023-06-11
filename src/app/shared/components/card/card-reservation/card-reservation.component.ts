bcb2aef67c3b6dd9d1de85a5deb9e5347a26f580
DChelo <lucumichelo2003gmail.com>
2023-06-10 19:50:11 -0500

    cambios card calificaciones

--- src/app/shared/components/card/card-reservation/card-reservation.component.ts
+++ src/app/shared/components/card/card-reservation/card-reservation.component.ts
@@ -8,14 +8,23 @@ import { Booking } from "src/app/interface/ibooking";
   styleUrls: ['./card-reservation.component.scss'],
 })
 export class CardReservationComponent {
+
+  selectedRating!: number;
   @Input() booking!: Booking;
   @Input() formattedInitialDate!: string | null;
   @Input() formattedFinalDate!: string | null;
+
+  submitRating() {
+    console.log('Calificación seleccionada:', this.selectedRating);
+    // Aquí puedes realizar más acciones con la calificación seleccionada, como enviarla al servidor, etc.
+  }
+
   public isOpenModal = false;
 
   public onTapRate(rating: { rate: number }) {
     console.log('rate', rating);
-    this.isOpenModal=true;
+    this.selectedRating = rating.rate;
+    this.isOpenModal = true;
   }
 
   loadImg(imgUrl : string){
